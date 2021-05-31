0010c3e6e5dff8a24b56d3f5428c0178c94c2054
RivinSainudeen <mohammedrivin@gmail.com>
2021-05-31 22:22:55 +0530

    Assignment done- 1st attempt

--- estimate.py
+++ estimate.py
@@ -1,32 +1,21 @@
+import random
 import math
-import unittest
 
-class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-
-
-class TestMC(unittest.TestCase):
-    def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
-        
-        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
-
-        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
-
-    def test_accuracy(self):
-        for i in range(500, 600):
-            pi = monte_carlo(i)
-            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
-if __name__ == "__main__":
-    unittest.main()
+def wallis(n):
+	t = 1 
+	for i in range (1,n):
+		t = t * 4 * (n^2) / (4*(n^2) - 1)
+	return t*2
+	
+def monte_carlo(n):
+	i = 0 #iteration counter
+	c = 0 #points inside circle - counter
+	s = 0 #points outside circle - counter
+	while i <= n:
+		xy = [random.random(), random.random()] #random co-ordinates
+		if math.sqrt(xy[0] ^2 + xy[1] ^2) < 1:
+			c = c + 1
+		else:
+			s = s + 1
+	return 4 * c / (c+s)
+	
