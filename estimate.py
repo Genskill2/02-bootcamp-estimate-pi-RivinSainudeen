a7ed3d27edce1a703c8f1457de9b75feb4fc370a
RivinSainudeen <mohammedrivin@gmail.com>
2021-06-01 18:24:05 +0530

    Assignment done attemt#2

--- estimate.py
+++ estimate.py
@@ -3,19 +3,22 @@ import math
 
 def wallis(n):
 	t = 1 
-	for i in range (1,n):
-		t = t * 4 * (n^2) / (4*(n^2) - 1)
-	return t*2
+	for i in range (1,n+1):
+		t = t * 4 * (i*i) / (4*(i*i) - 1)
+	return t * 2
 	
 def monte_carlo(n):
-	i = 0 #iteration counter
+	i = 0
 	c = 0 #points inside circle - counter
 	s = 0 #points outside circle - counter
 	while i <= n:
 		xy = [random.random(), random.random()] #random co-ordinates
-		if math.sqrt(xy[0] ^2 + xy[1] ^2) < 1:
+		r = math.sqrt(xy[0] * xy[0] + xy[1] * xy[1]) 
+		if r < 1:
 			c = c + 1
+			s = s + 1
 		else:
 			s = s + 1
-	return 4 * c / (c+s)
+		i = i + 1
+	return 2 * 4 * c / (c+s)
 	
