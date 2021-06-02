3bec51d30d831fd0a9de770cf7f6af84c0b964bc
RivinSainudeen <mohammedrivin@gmail.com>
2021-06-02 11:42:09 +0530

    Assignment done. Third attempt

--- estimate.py
+++ estimate.py
@@ -2,23 +2,24 @@ import random
 import math
 
 def wallis(n):
-	t = 1 
+	t = 1.0 
 	for i in range (1,n+1):
-		t = t * 4 * (i*i) / (4*(i*i) - 1)
-	return t * 2
+		t = t * 4.0 * (i*i) / (4.0 * (i*i) - 1)
+	return t * 2.0
 	
 def monte_carlo(n):
 	i = 0
 	c = 0 #points inside circle - counter
-	s = 0 #points outside circle - counter
+	s = 0 #points inside square - counter
 	while i <= n:
-		xy = [random.random(), random.random()] #random co-ordinates
-		r = math.sqrt(xy[0] * xy[0] + xy[1] * xy[1]) 
-		if r < 1:
+		xy = [random.randrange(-1,1), random.randrange(-1,1)] #random co-ordinates
+		r = xy[0]**2 + xy[1]**2
+		r = math.sqrt(r)
+		if r <= 1:
 			c = c + 1
 			s = s + 1
 		else:
 			s = s + 1
 		i = i + 1
-	return 2 * 4 * c / (c+s)
+	return 4 * float(c) / float(s)
 	
